32e4df9520628b0fcc2a1ef0b644e6df0581d0b7
DanTel <DaniyarT2005@gmail.com>
2025-04-21 20:39:50 +0500

    feat: add Country and ProductionCompany models to backend

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    flag = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.flag} {self.name}"
+
+class ProductionCompany(models.Model):
+    name = models.CharField(max_length=100)
+    headquarters = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.name
+
 class Movie(models.Model):
   title = models.CharField(max_length=100)
   description = models.TextField()
@@ -12,6 +26,8 @@ class Movie(models.Model):
   rating = models.FloatField()
   poster_url = models.CharField(max_length=255)
   trailer_url = models.CharField(max_length=255)
+  country = models.ForeignKey(Country, on_delete=models.CASCADE, null=True, related_name='movies')
+  production_company = models.ForeignKey(ProductionCompany, on_delete=models.SET_NULL, null=True, related_name='movies')
 
   def __str__(self):
     return self.title
